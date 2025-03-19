b5bf406cb470ee5fef53e2cc30fbc35a8720fc62
ankit0318 <ankitsharma0318@gmail.com>
2025-03-19 23:30:34 +0530

    main

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0', // Allows access from other devices
     port: 5173 // Or any desired port
